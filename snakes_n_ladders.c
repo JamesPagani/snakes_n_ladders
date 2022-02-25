41873da7f51b100a126e845f32a16a55eedcdc09
JamesPagani <jaimeagalvezv@gmail.com>
2022-02-25 11:14:48 -0500

    Finishing the first version
    Finished a rough version of the game, which will work with boards with 25 spaces.

--- snakes_n_ladders.c
+++ snakes_n_ladders.c
@@ -1,24 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
-/** snakes_n_ladders - Plays a game of snakes and ladders
+#include <time.h>
+/** 
+ * snakes_n_ladders - Plays a game of snakes and ladders
  * @num_players: Amount of players.
+ * @board_size: Size of the board.
  */
 
-void snakes_n_ladders(int num_players)
+void snakes_n_ladders(int num_players, int board_size)
 {
 	int isDone = 0;
 	int die;
-	int *players;
+	int *position;
+	int *board;
 
 	if (num_players <= 0)
 	{
 		printf("You need at least one player to play this game!");
 		return;
 	}
+	if (board_size <= 0)
+	{
+		printf("Board size has to be bigger than 0!");
+		return;
+	}
+	position = calloc(num_players, sizeof(int));
+	board = calloc(board_size + 1, sizeof(int));
 
-	players = calloc(num_players, sizeof(int));
+	board[3] = 11;
+	board[6] = 17;
+	board[9] = 18;
+	board[10] = 12;
+	board[14] = 4;
+	board[19] = 8;
+	board[22] = 20;
+	board[24] = 16;
 
 	/* Play the game until someone finishes */
+	srand(time(NULL));
 	while(!isDone)
 	{
 		/* Move all players per turn */
@@ -26,24 +45,39 @@ void snakes_n_ladders(int num_players)
 		{
 			die = 1 + rand() / ((RAND_MAX + 1u) / 6);
 			printf("Player %d rolled %d\n", p + 1, die);
-			players[p] += die;
-			printf("Player %d landed on square %d\n", p + 1, players[p]);
-			if (players[p] >= 25)
+			position[p] += die;
+			printf("Player %d landed on square %d\n", p + 1, position[p]);
+			if (position[p] >= board_size)
 			{
 				isDone = 1;
+				printf("Player %d won!\n", p + 1);
 				break;
+			}
+			if (board[position[p]] != 0)
+			{
+				if (board[position[p]] < position[p])
+				{
+					printf("Player %d went down a snake, landing on square %d\n", p + 1, board[position[p]]);
+				}
+				else
+				{
+					printf("Player %d went up a ladder, climbing up to square %d\n", p + 1, board[position[p]]);
+				}
+				position[p] = board[position[p]];
 			}
 		}
 	}
 
-	free(players);
+	free(position);
+	free(board);
 }
 
-/** main - Entry point
+/** 
+ * main - Entry point
  * Return: 1 on success, any value on failure.
  */
 int main()
 {
-	snakes_n_ladders(2);
+	snakes_n_ladders(2, 25);
 	return 1;
 }
